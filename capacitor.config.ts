e5c22e4b6da361518ce1272bfeaf69ccc0d612f6
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-05-17 15:07:39 -0500

    Updates

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'Nexus',
-  webDir: 'www'
-};
-
-export default config;
